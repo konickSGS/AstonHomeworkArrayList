13b993210c3a84707d414a9f8eb84ae89803ba25
konickSGS <konick.gevurah@gmail.com>
2024-09-08 19:16:06 +0300

    Переделка shiftRightNoCircle для работы с отрицательным shift

--- src/main/java/gs/konick/util/ArrayUtil.java
+++ src/main/java/gs/konick/util/ArrayUtil.java
@@ -7,11 +7,13 @@ import java.util.Arrays;
  */
 public class ArrayUtil {
     /**
-     * Сдвиг массива вправо, но не циклический.
+     * Сдвиг массива вправо, но не циклический. shift может быть отрицательным
      * Возьмем массив:
      * 0 1 2 3 4 5 6 7 8 9
      * Результат shiftNoCircle(array, 4, 2):
      * 0 1 2 3 4 5 4 5 6 7
+     * Результат shiftNoCircle(array, 4, -2):
+     * 0 1 4 5 6 7 8 9 8 9
      * @param array - исходный массив
      * @param indexFrom - индекс с которого мы хотим двигать
      * @param shift - сдвиг. Только положительное
@@ -19,13 +21,19 @@ public class ArrayUtil {
      * @throws IllegalArgumentException
      */
     public static Object[] shiftRightNoCircle(Object[] array, int indexFrom, int shift) {
-        if (shift < 0) {
-            throw new IllegalArgumentException("Размер сдвига должен быть положительным");
-        }
-
         Object[] newArray = Arrays.copyOf(array, array.length);
 
-        for (int i = indexFrom; i < array.length - shift; i++) {
+        // Если shift отрицательный, то начало того куска, который мы хотим перенести, может уйти за начало массива
+        // И чтобы этого не было, мы сразу это начало отсекаем.
+        //    | 0 1 2 3 4 5 6 7 8 9. с indexFrom = 3 и shift = -4
+        //  3 | 4 5 6 7 8 9 6 7 8 9
+        // Мы видим, что 3 уехало на индекс -1, что приведет к ошибке. Поэтому indexFrom мы сдвигаем на 1
+        if (indexFrom + shift < 0) {
+            indexFrom += Math.abs(indexFrom + shift);
+        }
+        // Если shift отрицательное, то значит, что новый массив должен идти только до конца длины
+        int lastIndex = (shift >= 0) ? array.length - shift : array.length;
+        for (int i = indexFrom; i < lastIndex; i++) {
             newArray[i + shift] = array[i];
         }
         return newArray;
